ab2bcf87841da4fa112ec80f4d48604209863252
Nasiruddin Abubakar <nasiruddinabubakar@gmail.com>
2023-08-09 00:07:55 +0500

    featured products done with responsive

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,8 +6,8 @@ export default function App() {
   return (
             <>
                 <Header />
-                <SingleProduct />
-                {/* <FeaturedProducts /> */}
+                {/* <SingleProduct /> */}
+                <FeaturedProducts />
                
 
             </>
--- src/components/FeaturedProduct.tsx
+++ src/components/FeaturedProduct.tsx
@@ -1,29 +1,75 @@
+import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
+const imageArray = [
+  "/products/f1.jpg",
+  "/products/f2.jpg",
+  "/products/f3.jpg",
+  "/products/f4.jpg",
+  "/products/f5.jpg",
+  "/products/f6.jpg",
+  "/products/f7.jpg",
+  "/products/f8.jpg",
+  "/products/n1.jpg"
+];
 
 export default function FeaturedProducts() {
-  return <div className="featured__products">
-    <FeaturedView />
-    <FeaturedView />
-    <FeaturedView />
-    <FeaturedView />
-    <FeaturedView />
-    <FeaturedView />
-    <FeaturedView />
-    <FeaturedView />
-  </div>;
-}
-export function FeaturedView() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products?limit=10"
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    }
+
+    getProducts();
+  }, []);
   return (
-    <div className="single__product" >
-      <div className="banner__image">
-        <img src="/products/f1.jpg" alt="fa1"></img>
+    <>
+      <Heading />
+      <div className="featured__products">
+        {data.map((item, i) => (
+          <FeaturedView key={i} item={item} i={i+1} />
+        ))}
       </div>
-     <div className="details__feature__product">
-        <h2>Shilva T-shirt</h2>
-        <h3> 132$</h3>
+    </>
+  );
+}
+interface FeaturedViewProps {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: any[];
+  title: string;
+}
+// let i:number = 1;
+export function FeaturedView({ item,i }: { item: FeaturedViewProps, i:number }) {
+ 
+  return (
+    <div className="single__product">
+      <div className="banners__image">
+        <img src={item.image} alt="fa1"></img>
+      </div>
+      <div className="details__feature__product">
+        <h3>{item.title}</h3>
+        <h4>${item.price}</h4>
         {/* <StarRating maxRating={5} /> */}
-      </div> 
+      </div>
     </div>
   );
 }
-  
+export function Heading() {
+  return (
+    <div className="featured__heading">
+      <h1>Featured Products</h1>
+    </div>
+  );
+}
