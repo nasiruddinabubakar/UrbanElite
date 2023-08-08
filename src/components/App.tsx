8066fa429bea3b5fc9e38211cb5b387a16e87442
Nasiruddin Abubakar <nasiruddinabubakar@gmail.com>
2023-08-09 00:09:07 +0500

    featured porducts responsive

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
             <>
                 <Header />
-                {/* <SingleProduct /> */}
+                <SingleProduct />
                 <FeaturedProducts />
                
 
