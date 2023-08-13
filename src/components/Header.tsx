4159f4455a83f9ee0db3c2d254bba508cd1d4482
Nasiruddin Abubakar <nasiruddinabubakar@gmail.com>
2023-08-14 01:38:52 +0500

    Header arr done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,65 +1,72 @@
-import { faBolt,faUser,faCartShopping,faHouse,faSearch} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBolt,
+  faUser,
+  faCartShopping,
+  faHouse,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
+
+interface NavItem {
+  icon: IconDefinition; // This is where you store FontAwesome icons
+  text: string;
+}
+
+const navArray: NavItem[] = [
+  { icon: faHouse, text: "Home" },
+  { icon: faBolt, text: "Sale" },
+  { icon: faUser, text: "Account" },
+  { icon: faCartShopping, text: "Cart" },
+];
 
 export default function Header() {
   return (
     <header>
-      <div>
-        <h1>UrbanElite</h1>
-      </div>
-      <div className="searchh">
-        <input type="text" placeholder="Search Here" />
-        <button>
-        <i>
-                <FontAwesomeIcon icon={faSearch} />
-              </i>
-        </button>
-      </div>
+      <Logo />
+      <Search />
       <nav className="navlist">
         <ul className="navli">
-        {/* <li>
-            <div className="nav__div">
-              <i>
-                <FontAwesomeIcon icon={faSearch} />
-              </i>
-              <label>Search</label>
-            </div>
-          </li> */}
-          <li>
-            <div className="nav__div">
-              <i>
-                <FontAwesomeIcon icon={faHouse} />
-              </i>
-              <label>Home</label>
-            </div>
-          </li>
-          <li>
-            <div className="nav__div">
-              <i>
-                <FontAwesomeIcon icon={faBolt} />
-              </i>
-              <label>Sale</label>
-            </div>
-          </li>
-          <li>
-            <div className="nav__div">
-              <i>
-                <FontAwesomeIcon icon={faUser} />
-              </i>
-              <label>Account</label>
-            </div>
-          </li>
-          <li>
-            <div className="nav__div">
-              <i>
-                <FontAwesomeIcon icon={faCartShopping} />
-              </i>
-              <label>Cart</label>
-            </div>
-          </li>
+          {navArray.map((item) => (
+            <ListComponents item={item} />
+          ))}
         </ul>
       </nav>
     </header>
+  );
+}
+function Logo() {
+  return (
+    <div>
+      <h1>UrbanElite</h1>
+    </div>
+  );
+}
+function Search() {
+  return (
+    <div className="searchh">
+      <input type="text" placeholder="Search Here" />
+      <button>
+        <i>
+          <FontAwesomeIcon icon={faSearch} />
+        </i>
+      </button>
+    </div>
+  );
+}
+interface Nav {
+  item: NavItem;
+}
+export function ListComponents({ item }: Nav) {
+  return (
+    <li>
+      <div className="nav__div">
+        <i>
+          <FontAwesomeIcon icon={item.icon} />
+        </i>
+        <label>{item.text}</label>
+      </div>
+    </li>
   );
 }
